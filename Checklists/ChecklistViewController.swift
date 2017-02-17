8772dcbeb9be9c86ef6b3811fb9c87fbe08d038a
Tanya Tomchuk <tanyem@protonmail.com>
2017-02-17 20:56:34 +0300

    Added tableView(didSelectRowAt) and Table View have 100 rows

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -21,26 +21,51 @@ class ChecklistViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return 100
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItemCell", for: indexPath)
         
         let label = cell.viewWithTag(1000) as! UILabel
-        if indexPath.row == 0 {
-            label.text = "Walk the dog 1"
-        } else if indexPath.row == 1 {
+//        if indexPath.row == 0 {
+//            label.text = "Cook breakfast"
+//        } else if indexPath.row == 1 {
+//            label.text = "Brush my teeth"
+//        } else if indexPath.row == 2 {
+//            label.text = "Learn iOS development"
+//        } else if indexPath.row == 3 {
+//            label.text = "Workout"
+//        } else if indexPath.row == 4 {
+//            label.text = "Learn English language"
+//        }
+        
+        if indexPath.row % 5 == 0 {
+            label.text = "Cook breakfast"
+        } else if indexPath.row % 5 == 1 {
             label.text = "Brush my teeth"
-        } else if indexPath.row == 2 {
+        } else if indexPath.row % 5 == 2 {
             label.text = "Learn iOS development"
-        } else if indexPath.row == 3 {
-            label.text = "Soccer practice"
-        } else if indexPath.row == 4 {
-            label.text = "Eat ice cream"
+        } else if indexPath.row % 5 == 3 {
+            label.text = "Workout"
+        } else if indexPath.row % 5 == 4 {
+            label.text = "Learn English language"
+        }
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if let cell = tableView.cellForRow(at: indexPath) {
+            if cell.accessoryType == .none {
+                cell.accessoryType = .checkmark
+            } else {
+                cell.accessoryType = .none
+            }
         }
 
-        return cell
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 
 }
