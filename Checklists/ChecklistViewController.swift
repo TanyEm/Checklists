fc1c30c56b05a833756a115ebb60a0058ade8819
Tanya Tomchuk <tanyem@protonmail.com>
2017-02-20 19:39:35 +0300

    Add configureCheckmark and instance variable in data model

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -9,6 +9,16 @@
 import UIKit
 
 class ChecklistViewController: UITableViewController {
+    var row0text = "Cook breakfast"
+    var row1text = "Brush teeth"
+    var row2text = "Learn iOS development"
+    var row3text = "Workout"
+    var row4text = "Learn English language"
+    var row0checked = false
+    var row1checked = false
+    var row2checked = false
+    var row3checked = false
+    var row4checked = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,37 +31,27 @@ class ChecklistViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 100
+        return 5
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItemCell", for: indexPath)
         
         let label = cell.viewWithTag(1000) as! UILabel
-//        if indexPath.row == 0 {
-//            label.text = "Cook breakfast"
-//        } else if indexPath.row == 1 {
-//            label.text = "Brush my teeth"
-//        } else if indexPath.row == 2 {
-//            label.text = "Learn iOS development"
-//        } else if indexPath.row == 3 {
-//            label.text = "Workout"
-//        } else if indexPath.row == 4 {
-//            label.text = "Learn English language"
-//        }
         
-        if indexPath.row % 5 == 0 {
-            label.text = "Cook breakfast"
-        } else if indexPath.row % 5 == 1 {
-            label.text = "Brush my teeth"
-        } else if indexPath.row % 5 == 2 {
-            label.text = "Learn iOS development"
-        } else if indexPath.row % 5 == 3 {
-            label.text = "Workout"
-        } else if indexPath.row % 5 == 4 {
-            label.text = "Learn English language"
+        if indexPath.row == 0 {
+            label.text = row0text
+        } else if indexPath.row == 1 {
+            label.text = row1text
+        } else if indexPath.row == 2 {
+            label.text = row2text
+        } else if indexPath.row == 3 {
+            label.text = row3text
+        } else if indexPath.row == 4 {
+            label.text = row4text
         }
         
+        configureCheckmark(for: cell, at: indexPath)
         return cell
     }
     
@@ -66,6 +66,28 @@ class ChecklistViewController: UITableViewController {
         }
 
         tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func configureCheckmark(for cell: UITableViewCell, at indexPath: IndexPath) {
+        var isChecked = false
+        
+        if indexPath.row == 0 {
+            isChecked = row0checked
+        } else if indexPath.row == 1 {
+            isChecked = row1checked
+        } else if indexPath.row == 2 {
+            isChecked = row2checked
+        } else if indexPath.row == 3 {
+            isChecked = row3checked
+        } else if indexPath.row == 4 {
+            isChecked = row4checked
+        }
+        
+        if isChecked {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
     }
 
 }
