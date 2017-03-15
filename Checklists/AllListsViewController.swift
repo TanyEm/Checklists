74effc2dff6a0cd50bbd24517c40d4ee0b62eaca
Tanya Tomchuk <tanyem@protonmail.com>
2017-03-15 22:09:20 +0300

    Added - new view controller Checklists and new class AllListsViewController - in Checklist.swift using the NSCoder system to load and save these objects There are add rows using code

---
+++ Checklists/AllListsViewController.swift
@@ -0,0 +1,82 @@
+//
+//  AllListsViewController.swift
+//  Checklists
+//
+//  Created by Tanya Tomchuk on 15.03.17.
+//  Copyright © 2017 Tanya Tomchuk. All rights reserved.
+//
+
+import UIKit
+
+class AllListsViewController: UITableViewController {
+    var lists = [Checklist]()
+    
+    required init?(coder aDecoder: NSCoder) {
+        lists = [Checklist]()
+        
+        super.init(coder: aDecoder)
+        
+        var list = Checklist(name: "Birthdays")
+        lists.append(list)
+        
+        list = Checklist(name: "Groceries")
+        lists.append(list)
+        
+        list = Checklist(name: "Cool Apps")
+        lists.append(list)
+        
+        list = Checklist(name: "To Do")
+        lists.append(list)
+        
+//        loadChecklistItems()
+        
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return lists.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = makeCell(for: tableView)
+        let checklist = lists[indexPath.row]
+        cell.textLabel!.text = checklist.name
+        cell.accessoryType = .detailDisclosureButton
+        // Configure the cell...
+
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "ShowChecklist", sender: nil)
+    }
+    
+    func makeCell(for tableView: UITableView) -> UITableViewCell {
+        let cellIdentifier = "Cell"
+        if let cell =
+            tableView.dequeueReusableCell(withIdentifier: cellIdentifier) {
+            return cell
+        } else {
+            return UITableViewCell(style: .default, reuseIdentifier: cellIdentifier)
+        }
+    }
+
+}
---
+++ Checklists/Checklist.swift
@@ -0,0 +1,19 @@
+//
+//  Checklist.swift
+//  Checklists
+//
+//  Created by Tanya Tomchuk on 15.03.17.
+//  Copyright © 2017 Tanya Tomchuk. All rights reserved.
+//
+
+import UIKit
+
+class Checklist: NSObject {
+    var name = ""
+    
+    init(name: String) {
+        self.name = name
+        super.init()
+    }
+
+}
