e42cb2c308b19b372d487bb9033da08eba53cfec
Tanya Tomchuk <tanyem@protonmail.com>
2017-03-27 17:05:35 +0300

    Improving the data model Added new file, there are methods saving and loading, contains the array of Checklist objects

--- Checklists/AppDelegate.swift
+++ Checklists/AppDelegate.swift
@@ -13,10 +13,14 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    let dataModel = DataModel()
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let navigationController = window!.rootViewController as! UINavigationController
+        let controller = navigationController.viewControllers[0] as! AllListsViewController
+        controller.dataModel = dataModel
         return true
     }
 
@@ -91,9 +95,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func saveData() {
-        let navigationController = window!.rootViewController as! UINavigationController
-        let controller = navigationController.viewControllers[0] as! AllListsViewController
-        controller.saveChecklists()
+        dataModel.saveChecklists()
     }
 }
 
