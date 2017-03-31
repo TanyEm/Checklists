6c12929ae3f1ce8a5444128dfa2a693e2d143c8f
Tanya Tomchuk <tanyem@protonmail.com>
2017-03-31 16:46:56 +0300

    Added sorting the lists

--- Checklists/DataModel.swift
+++ Checklists/DataModel.swift
@@ -40,6 +40,7 @@ class DataModel {
             let unarchiver = NSKeyedUnarchiver(forReadingWith: data)
             lists = unarchiver.decodeObject(forKey: "Checklists") as! [Checklist]
             unarchiver.finishDecoding()
+            sortChecklists()
         }
     }
     func registerDefaults() {
@@ -66,5 +67,11 @@ class DataModel {
             indexOfSelectedChecklist = 0
             userDefaults.set(false, forKey: "FirstTime")
             userDefaults.synchronize()
-        } }
+        }
+    }
+    
+    func sortChecklists() {
+        lists.sort(by: { checklist1, checklist2 in
+            return checklist1.name.localizedStandardCompare(checklist2.name) == .orderedAscending })
+    }
 }
